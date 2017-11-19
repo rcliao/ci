0a2dbecce028106ec7910466528ab1f5ae7b97d7
Eric Liao <rcliao01@gmail.com>
2017-11-19 10:39:37 -0800

    feat: add main method to define life cycle

--- e2etest.go
+++ e2etest.go
@@ -1,9 +1,13 @@
 package e2etest
 
+// Result represents the test result
 type Result struct {
-	Pass bool
+	Pass        bool
+	Description string
+	Context     string
 }
 
+// Status is DTO object to combine commit status with its result
 type Status struct {
 	ID          string
 	State       string
@@ -12,14 +16,56 @@ type Status struct {
 	Context     string
 }
 
+// Main is the main method to run though pipeline
+func Main(pipeline Pipeline, url, hash string) (Result, error) {
+	result := Result{}
+
+	err := pipeline.Clone(url)
+	if err != nil {
+		return result, err
+	}
+
+	buildCommand := "./scripts/build.sh"
+	err = pipeline.Build(buildCommand)
+	if err != nil {
+		result.Description = "failed to build repository"
+		result.Context = err.Error()
+		return result, nil
+	}
+
+	env := pipeline.Env()
+
+	stop := make(chan bool, 1)
+	startCommand := "./scripts/start.sh"
+	err = pipeline.Start(env, startCommand, stop)
+	defer func() {
+		stop <- true
+	}()
+	if err != nil {
+		result.Description = "failed to start application"
+		result.Context = err.Error()
+		return result, nil
+	}
+
+	result, err = pipeline.Test(env)
+	if err != nil {
+		result.Description = "failed to start test"
+		result.Context = err.Error()
+		return result, nil
+	}
+	return result, nil
+}
+
+// Pipeline defines each step in the test
 type Pipeline interface {
 	Clone(URL string) error
 	Build(command string) error
-	Env(defaultEnv []string) []string
-	Start(Env []string) error
+	Env() []string
+	Start(Env []string, command string, stop <-chan bool) error
 	Test(Env []string) (Result, error)
 }
 
+// StatusDAO defines the interaction status with DB
 type StatusDAO interface {
 	Update(status Status) error
 	Store(status Status) error
